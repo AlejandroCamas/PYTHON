7aa7d9ec57fde95d9fa11b6e81db15fe7e86defd
Alejandro Camas <alejandromcamas@gmail.com>
2023-04-03 21:05:35 -0600

    formas de concatener strings

--- STRINGSclase9.py
+++ STRINGSclase9.py
@@ -18,5 +18,16 @@ comillas_simples = "i'm Alejandro" #usamos las dobles comillas para que python s
 comillas_dobles = ' dice "ser muy pro"' #usamos las comillas dobles ya que usamos las dobles para hacer mencion de lo que alguien dijo.
 
 #ahora haremos la concatenacion de las variables para poder generar un texto:
-concatenacion = "hola mi nombre es " + name + " " + "y mi apellido es" + " " + last_name
-print(concatenacion)
+#forma 1
+concatenacion = "hola mi nombre es " + name + " " + "y  mi apellido es" + " " + last_name
+print("forma 1", concatenacion)
+
+#forma 2
+concatenacion = "hola mi nombre es {} y mi apellido {}".format(name, last_name)
+print("forma 2", concatenacion)
+
+#forrma 3
+concatenacion = f"hola mi nombre es {name} y mi apellido es {last_name}"
+print("v3", concatenacion)
+
+#como vemos son diferentes en codigo pero las tres hacen exacatamente lo mismo.
