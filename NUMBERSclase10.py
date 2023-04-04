70ad5c8cef45c0f0038b97d7761edf2931b03381
Alejandro Camas <alejandromcamas@gmail.com>
2023-04-03 22:02:50 -0600

    suma de numeros y strings

---
+++ NUMBERSclase10.py
@@ -0,0 +1,11 @@
+#En este codigo aprenderemos el uso de  los numeros en python
+
+#comenzamos a colocar nuestras variables.
+variable1 = 10
+variable2 = 4
+print("El numero en la variable 1 es ", variable1)
+
+print("El numero en la variable 2 es ", variable2)
+
+#Lo que realizamos fue la concatenacion de texto y numeros (los numeros en forma de variable).
+#Que pasa si lo que queremos es que el usuario nos indique su numero o su variable?
