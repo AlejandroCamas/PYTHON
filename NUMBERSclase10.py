8f1969a81e971b66103df325161f3e159acea54a
Alejandro Camas <alejandromcamas@gmail.com>
2023-04-03 22:15:47 -0600

    pedio o solicitar datos de entrada.

--- NUMBERSclase10.py
+++ NUMBERSclase10.py
@@ -9,3 +9,6 @@ print("El numero en la variable 2 es ", variable2)
 
 #Lo que realizamos fue la concatenacion de texto y numeros (los numeros en forma de variable).
 #Que pasa si lo que queremos es que el usuario nos indique su numero o su variable?
+#usamos un input:
+Usuario1 = input ("Cual numero toma?")
+#De esta manera indicamos al usuario que debe dar un numero como dato de entrada.
